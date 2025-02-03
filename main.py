ce7f3919ad7e5823ed1e26fb76cda74193e312e3
n0z0a <ibarra.a.noah@gmail.com>
2025-02-03 13:06:58 -0600

    Added JSON Words

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#TEMP
+import json
+
+with open("words_meanings.json", "r") as file:
+    data = json.load(file)  # Load JSON data
+
+if "words" in data and isinstance(data["words"], list):
+    first_five_words = [entry["word"] for entry in data["words"][:5]]
+    print(first_five_words)
+else:
+    print("Invalid JSON structure.")
